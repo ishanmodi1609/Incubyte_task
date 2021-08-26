f624dae8f9899b07afcf7242420554d325dc8afd
ishan <ishan123@gmail.com>
2021-08-26 22:00:26 +0530

    Adding 2 numbers involved in string pass the test

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -7,6 +7,15 @@ public class calculator {
             //This checks if string is empty than return 0
             return 0;
         }
+        else if(test.contains(",")){
+
+            //If string contains elimiter than it first converts string into 2 tokens among delimiter
+            // Ex:- "1,2" this converts into tokesn "1" and "2"
+            String tokens[]=test.split(",");
+
+            //both tokesn that is stored in an array gets converted into integr and then added
+            return Integer.parseInt(tokens[0])+Integer.parseInt(tokens[1]);
+        }
         else{
 
             //If string contains digit then converts string to int and then returns that int
