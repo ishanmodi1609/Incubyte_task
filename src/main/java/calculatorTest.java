68aa6532285a06a83f64077404d780ae5fedc030
ishan <ishan123@gmail.com>
2021-08-26 21:56:12 +0530

    Adding 2 numbers involved in string does not pass the test case

--- src/main/java/calculatorTest.java
+++ src/main/java/calculatorTest.java
@@ -15,4 +15,11 @@ public class calculatorTest {
     public void shouldReturnNumber(){
         Assert.assertEquals(2,calculator.add("2"));
     }
+
+    //This is the function to add 2 numbers involved in the string
+    //Ans should be sum of those 2 nummbers
+    @Test
+    public void shouldReturnSumNumber(){
+        Assert.assertEquals(3,calculator.add("1,2"));
+    }
 }
