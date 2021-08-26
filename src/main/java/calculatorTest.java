6181aa4787bb32d8d16852745a2737521594244e
ishan <ishan123@gmail.com>
2021-08-26 21:48:12 +0530

    First Test case does pass on empty string and return zero

--- src/main/java/calculatorTest.java
+++ src/main/java/calculatorTest.java
@@ -1,13 +1,11 @@
 import org.junit.*;
 
 public class calculatorTest {
-    @Test
 
     //This is the function to check output on empty string
     //Ans should be zero
-
-
-    public void shouldReturnZeroEmpty(String s){
+    @Test
+    public void shouldReturnZeroEmpty(){
         Assert.assertEquals(0,calculator.add(""));
     }
 }
