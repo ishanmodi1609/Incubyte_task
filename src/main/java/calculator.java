b17c464d7ce355d83eee1667a705e2d919bd9f87
ishan <ishan123@gmail.com>
2021-08-26 21:53:56 +0530

    String involving only one number does passes the test case

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -2,6 +2,15 @@ public class calculator {
 
     //returns zero on any given input of string
     public static int add(String test){
-        return 0;
+        if(test.isEmpty()){
+
+            //This checks if string is empty than return 0
+            return 0;
+        }
+        else{
+
+            //If string contains digit then converts string to int and then returns that int
+            return Integer.parseInt(test);
+        }
     }
 }
--- src/main/java/calculatorTest.java
+++ src/main/java/calculatorTest.java
@@ -9,6 +9,8 @@ public class calculatorTest {
         Assert.assertEquals(0,calculator.add(""));
     }
 
+    //This is the function to check output on one digit string
+    //Ans should be that number
     @Test
     public void shouldReturnNumber(){
         Assert.assertEquals(2,calculator.add("2"));
