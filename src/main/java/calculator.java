cbe93b523e0745370e6e57f4eef4d1e5c527eee4
ishan <ishan123@gmail.com>
2021-08-26 21:45:11 +0530

    First Test case does not pass on empty string

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,2 +1,7 @@
 public class calculator {
+
+    //returns zero on any given input of string
+    public static  int add(String test){
+        return -1;
+    }
 }
--- src/main/java/calculatorTest.java
+++ src/main/java/calculatorTest.java
@@ -1,2 +1,13 @@
+import org.junit.*;
+
 public class calculatorTest {
+    @Test
+
+    //This is the function to check output on empty string
+    //Ans should be zero
+
+
+    public void shouldReturnZeroEmpty(String s){
+        Assert.assertEquals(0,calculator.add(""));
+    }
 }
